ad31c6c5317ea9833386cc6ae9132de6f632fcec
Your Name <you@example.com>
2019-02-28 16:35:13 +0700

    timepicker

--- app/src/main/java/android/example/listme/MainActivity.java
+++ app/src/main/java/android/example/listme/MainActivity.java
@@ -2,12 +2,31 @@ package android.example.listme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TimePicker tp;
+    TextView tv;
+    TimePicker.OnTimeChangedListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.time_picker);
+        tp = (TimePicker) findViewById(R.id.timePicker1);
+        tv = (TextView) findViewById(R.id.textView1);
+
+        // listener jika terjadi perubahan isi TimePicker
+        TimePicker.OnTimeChangedListener listener = new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                Toast.makeText(MainActivity.this, "Masukkan waktu: " + hourOfDay + ":" + minute,
+                        Toast.LENGTH_SHORT).show();
+            }
+        };
+        // Menerapkan listener pada TimePicker tp
+        tp.setOnTimeChangedListener(listener);
     }
 }
